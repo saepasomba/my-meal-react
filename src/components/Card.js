caaf94b35b3e4c4a9f7a5d0a0d52bb25174a3d49
Sae Pasomba <saepasomba12@gmail.com>
2022-09-23 13:41:58 +0700

    Initial setup and navbar

---
+++ src/components/Card.js
@@ -0,0 +1,8 @@
+import React from 'react'
+import style from './Card.module.css'
+
+export default function Card() {
+  return (
+    <div>Hi</div>
+  )
+}
---
+++ src/components/Nav.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import style from './Nav.module.css'
+
+export default function Nav() {
+  return (
+    <nav>
+        <ul>
+            <li>Home</li>
+            <li>My favourite</li>
+        </ul>
+    </nav>
+  )
+}
